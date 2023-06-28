b05f97e499f22d0bf0cda96a12933374223c78cf
moroz-art-dev <moroz.art.dev@gmail.com>
2023-06-28 17:58:50 +0300

    AbortController and Timeout

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,5 @@
 import {TextField} from '@mui/material';
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 
 import {setSearchText, setSearchResults} from '../redux/features/searchSlice';
 import {useAppDispatch, useAppSelector} from '../redux/hooks';
@@ -8,11 +8,19 @@ const SearchForm = () => {
   const dispatch = useAppDispatch();
   const {searchText, limit} = useAppSelector(state => state.search);
 
+  const controllerRef = useRef<AbortController | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
+      // Create a new instance of AbortController
+      const controller = new AbortController();
+      controllerRef.current = controller;
+
       try {
         const res = await fetch(
-          `https://jsonplaceholder.typicode.com/photos?q=${searchText}&_limit=${limit}`
+          `https://jsonplaceholder.typicode.com/photos?q=${searchText}&_limit=${limit}`,
+          {signal: controller.signal}
         );
         const data = await res.json();
         console.log(data);
@@ -22,7 +30,33 @@ const SearchForm = () => {
       }
     };
 
-    fetchData();
+    // Cancel the previous request and reset the current controller
+    if (controllerRef.current) {
+      controllerRef.current.abort();
+      controllerRef.current = null;
+    }
+
+    // Cancel the previous timeout, if any
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    // Set a new timeout to execute the request after 500ms
+    timeoutRef.current = setTimeout(() => {
+      fetchData();
+      timeoutRef.current = null; // Reset the current timeout
+    }, 500);
+
+    // Clear the timeout and controller on component unmount
+    return () => {
+      if (controllerRef.current) {
+        controllerRef.current.abort();
+        controllerRef.current = null;
+      }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
   }, [searchText, limit, dispatch]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
